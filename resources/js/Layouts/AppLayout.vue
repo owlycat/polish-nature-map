733e87371c4bdcdb5356168467ac08a79f74275d
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-03 18:13:29 +0100

    Application layout with navigation

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import Toast from 'primevue/toast';
+import NavigationBar from "@/Components/NavigationBar.vue";
 
 defineProps({
     title: String,
@@ -8,8 +9,15 @@ defineProps({
 </script>
 
 <template>
-  <Head :title="title" />
-  <Toast position="bottom-right" />
-  
-  <slot />
+    <Head :title="title" />
+    <Toast position="bottom-right" />
+
+    <div class="flex flex-col h-screen">
+        <div class="sticky top-0 z-20 bg-blue-300">
+            <NavigationBar />
+        </div>
+        <div class="flex flex-col overflow-y-auto">
+            <slot />
+        </div>
+    </div>
 </template>
