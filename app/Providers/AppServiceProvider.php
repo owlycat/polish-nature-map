9101b31e2656c0f2e0f39cc1c1e1c04bd05f7caa
plencka <lencka.paulina@gmail.com>
2023-11-03 17:58:02 +0100

    CRS transform coordinates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Helpers\CoordinateReferenceSystemHelper;
+use App\Helpers\Facades\CoordinateReferenceSystemFacade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CoordinateReferenceSystemFacade::class, function(){
+            return new CoordinateReferenceSystemHelper();
+        });
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'CRS' => App\Helpers\Facades\CoordinateReferenceSystemFacade::class,
     ])->toArray(),
 
 ];
