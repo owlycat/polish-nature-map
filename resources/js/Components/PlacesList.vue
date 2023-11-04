02b670adb6e38d8cf23447aedb35c683a838c5ed
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-04 23:26:06 +0100

    Places list prototype

--- resources/js/Components/PlacesList.vue
+++ resources/js/Components/PlacesList.vue
@@ -2,21 +2,31 @@
 import Card from 'primevue/card';
 </script>
 <template>
-  <div class="bg-white rounded border md:max-w-md w-full md:w-96 flex flex-col h-full">
+  <div class="bg-white rounded border md:max-w-md w-full md:w-screen flex flex-col h-full">
     <Card class="sticky top-0 z-10">
       <template #title>
         (( Search component ))
       </template>
     </Card>
     <div class="overflow-y-auto flex-grow max-h-fit">
-      <ul>
-        <li
+        <div
           v-for="n in 100"
           :key="n"
         >
-          Item {{ n }}
-        </li>
-      </ul>
+          <div class="flex items-center bg-white rounded-lg shadow-md border overflow-hidden hover:bg-surface-100 p-3">
+            <img class="h-20 w-20 object-cover rounded-lg opacity-60" src="/images/place-default-thumbnail.png" alt="Place">
+
+            <div class="flex-grow p-2">
+              <h3 class="font-semibold text-lg">Some place {{ n }}</h3>
+              <p class="text-sm text-gray-600">National park</p>
+            </div>
+            <div>
+              <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-3 text-xs rounded transition-colors duration-200">
+                Visited
+              </button>
+            </div>
+          </div>
+        </div>
     </div>
     <Card class="sticky bottom-0 z-10">
       <template #title>
