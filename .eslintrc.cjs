cae58bfe5f2be66525a04f210649ca54989250eb
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-04 18:41:52 +0100

    Update ESLint configuration with new rules and globals

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
 module.exports = {
     extends: [
-      'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
     ],
+    globals: {
+        'route': 'readonly',
+        'axios': 'readonly',
+    },
     rules: {
-      'vue/no-unused-vars': 'error',
-      'vue/multi-word-component-names': "off",
-      'vue/require-default-prop': "off",
-      'eol-last': ['error', 'always'],
-      'vue/component-tags-order': ['error', {
-        'order': ['script', 'template', 'style'],
-      }],
-      quotes: ['error', 'single'],
+        'vue/no-unused-vars': 'error',
+        'vue/multi-word-component-names': "off",
+        'vue/require-default-prop': "off",
+        'eol-last': ['error', 'always'],
+        'vue/component-tags-order': ['error', {
+            'order': ['script', 'template', 'style'],
+        }],
+        quotes: ['error', 'single'],
     }
-  }
+}
