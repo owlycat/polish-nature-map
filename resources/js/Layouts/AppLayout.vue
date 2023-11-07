06b5563e099e6bcfa43739c0b698b3187f13554e
plencka <lencka.paulina@gmail.com>
2023-11-07 22:41:34 +0100

    Remove implementation for test broadcast

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,39 +1,11 @@
 <script setup>
-import { Head, usePage } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 import Toast from 'primevue/toast';
 import NavigationBar from '@/Components/NavigationBar.vue';
-import { useToast } from 'primevue/usetoast';
-import { ref, watchEffect } from 'vue';
 
-const toast = useToast();
-const inertiaPage = usePage();
 
 defineProps({
     title: String,
-});
-
-const user = ref(null);
-const subscription = ref(null);
-
-watchEffect(async () => {
-  user.value = inertiaPage.props.auth.user;
-
-  if (subscription.value != null) {
-    Echo.leaveChannel(subscription.value.name);
-    subscription.value = null;
-  }
-
-  if (user.value != null) {
-    subscription.value = Echo.channel(`user-message.${user.value.id}`)
-      .listen('SendMessageEvent', (e) => {
-        toast.add({
-          severity: e.messageType,
-          summary: 'New Message',
-          detail: e.message,
-          life: 5000,
-        });
-      });
-  }
 });
 </script>
 
