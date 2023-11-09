9ff4ce4c0f27d5282eb801625a41757aba286bcf
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-09 21:53:32 +0100

    Add map loading states

--- resources/js/Components/Maplibre/Map.vue
+++ resources/js/Components/Maplibre/Map.vue
@@ -2,19 +2,31 @@
 import { ref, onMounted, onBeforeUnmount} from 'vue';
 import { addControls } from './Partials/mapControls.js';
 import { loadGeojsonFeatures } from './Partials/loadGeojsonFeatures.js';
+import ProgressSpinner from 'primevue/progressspinner';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 import maplibregl from 'maplibre-gl';
 
+const MAPLIBRE_TOKEN = import.meta.env.VITE_MAPLIBRE_TOKEN;
 const mapContainer = ref(null);
 const map = ref(null);
 
-function createMap(){
+
+const LoadingStates = {
+    FetchingData: 'Fetching Data...',
+    PopulatingMap: 'Populating Map with Data...',
+    RenderingMap: 'Rendering Map...',
+    Completed: 'Completed',
+};
+
+const currentLoadingState = ref(LoadingStates.FetchingData);
+
+function createMap() {
     map.value = new maplibregl.Map({
         container: mapContainer.value,
-        style: `https://api.maptiler.com/maps/1d49ef3b-6f11-4b99-bc3e-5333cb10e6ab/style.json?key=${MAPLIBRE_TOKEN}`,
-        center: [0, 0],
-        zoom: 0,
+        style: `https://api.maptiler.com/maps/dataviz/style.json?key=${MAPLIBRE_TOKEN}`,
+        center: [18.907417071275205, 52.373746306181296],
+        zoom: 6,
         crossSourceCollisions: false,
         failIfMajorPerformanceCaveat: false,
         attributionControl: false,
@@ -24,18 +36,32 @@ function createMap(){
         maxPitch: 60,
     });
 
-    map.value.on('load', function () {
-        fetch('./assets/placeholder/output.geojson')
+    map.value.on('load', () => {
+    fetch('./assets/placeholder/output.geojson')
         .then(response => response.json())
         .then(geojson => {
-            loadGeojsonFeatures(map.value, geojson, 'placeholder', '#6321b5')
+            currentLoadingState.value = LoadingStates.PopulatingMap;
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    loadGeojsonFeatures(map.value, geojson, 'placeholder', '#6321b5');
+                    resolve();
+                }, 200);
+            });
+        })
+        .then(() => {
+            currentLoadingState.value = LoadingStates.RenderingMap;
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    addControls(map.value);
+                    resolve();
+                }, 200);
+            });
+        })
+        .finally(() => {
+            currentLoadingState.value = LoadingStates.Completed;
         });
-
-        addControls(map.value);
-    });
+});
 }
-
-const MAPLIBRE_TOKEN = import.meta.env.VITE_MAPLIBRE_TOKEN;
 
 onMounted(() => {
     createMap();
@@ -50,8 +76,11 @@ onBeforeUnmount(() => {
 });
 </script>
 <template>
-  <div
-    ref="mapContainer"
-    class="h-full w-full"
-  />
+    <div v-show="currentLoadingState !== LoadingStates.Completed" class="flex items-center justify-center w-full h-full">
+        <div class="flex flex-col gap-2">
+            <ProgressSpinner />
+            {{ currentLoadingState }}
+        </div>
+    </div>
+    <div v-show="currentLoadingState === LoadingStates.Completed" ref="mapContainer" class="h-full w-full"/>
 </template>
