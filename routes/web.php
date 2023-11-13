572603df996d567bf40b2a556d2a6eb1244a9f7d
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-13 16:32:50 +0100

    Remove unused components

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,6 @@ use Inertia\Inertia;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('index');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
-
 Route::group(['middleware' => [
     'auth:sanctum',
     config('jetstream.auth_session'),
