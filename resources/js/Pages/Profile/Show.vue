532f35d2c6f8f9cb92439ab50bc86e4cfb9a7e85
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-16 00:21:47 +0100

    Fix padding in profile page

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -17,7 +17,7 @@ defineProps({
 
 <template>
   <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto sm:py-10 sm:px-6 lg:px-8">
       <Panel header="User settings">
         <div v-if="$page.props.jetstream.canUpdateProfileInformation">
           <UpdateProfileInformationForm :user="$page.props.auth.user" />
