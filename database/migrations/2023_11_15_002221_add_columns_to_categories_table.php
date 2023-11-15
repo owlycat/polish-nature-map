1e37ce11b25b6e8d43273196ac27a2ed608710c2
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-15 02:12:02 +0100

    Refactor Categories.vue component

---
+++ database/migrations/2023_11_15_002221_add_columns_to_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('display_name')->nullable()->after('name');
+            $table->string('image')->nullable()->after('display_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('display_name');
+            $table->dropColumn('image');
+        });
+    }
+};
--- resources/js/Pages/Admin/Categories.vue
+++ resources/js/Pages/Admin/Categories.vue
@@ -3,51 +3,14 @@ import Layout from "@/Pages/Admin/Layout.vue";
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import InputText from 'primevue/inputtext';
+import { router } from '@inertiajs/vue3'
 
-import {ref} from "vue";                   // optional
+import {ref} from "vue"; 
 
-const categories = ref([
-  {
-    id: 1,
-    name: 'Category 1',
-    image: '/images/place-default-thumbnail.png',
-    display_name: 'Category 1',
-    created_at: '2021-08-01 12:00:00',
-    updated_at: '2021-08-01 12:00:00',
-  },
-  {
-    id: 2,
-    name: 'Category 2',
-    image: '/images/place-default-thumbnail.png',
-    display_name: 'Category 2',
-    created_at: '2021-08-01 12:00:00',
-    updated_at: '2021-08-01 12:00:00',
-  },
-  {
-    id: 3,
-    name: 'Category 3',
-    image: '/images/place-default-thumbnail.png',
-    display_name: 'Category 3',
-    created_at: '2021-08-01 12:00:00',
-    updated_at: '2021-08-01 12:00:00',
-  },
-  {
-    id: 4,
-    name: 'Category 4',
-    image: '/images/place-default-thumbnail.png',
-    display_name: 'Category 4',
-    created_at: '2021-08-01 12:00:00',
-    updated_at: '2021-08-01 12:00:00',
-  },
-  {
-    id: 5,
-    name: 'Category 5',
-    image: '/images/place-default-thumbnail.png',
-    display_name: 'Category 5',
-    created_at: '2021-08-01 12:00:00',
-    updated_at: '2021-08-01 12:00:00',
-  },
-]);
+const props = defineProps({
+    categories: Array,
+});
+
 
 const onRowEditSave = (event) => {
   let { newData, index } = event;
@@ -66,7 +29,7 @@ const editingRows = ref([]);
           <Column field="name" header="Name"></Column>
           <Column field="image" header="Image">
             <template #body="slotProps">
-              <img :src="`${slotProps.data.image}`" :alt="slotProps.data.image" class="w-12" />
+              <img :src="slotProps.data.image ? slotProps.data.image : '/images/place-default-thumbnail.png'" :alt="slotProps.data.image" :class="slotProps.data.image ? 'w-12' : 'grayscale w-12'" />
             </template>
           </Column>
           <Column field="display_name" header="Display name">
