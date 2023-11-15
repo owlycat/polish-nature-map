df0e6641bf44064d9d0848b008965f5aa43a361f
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-11-15 01:18:05 +0100

    Add more statistics

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -8,6 +8,7 @@ use App\Models\SpatialFeature;
 use App\Models\User;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Support\Facades\DB;
 
 class StatisticsController extends Controller
 {
@@ -18,15 +19,33 @@ class StatisticsController extends Controller
         ]);
     }
 
+    private function statistic(string $name, $value): array
+    {
+        return ['name' => $name, 'value' => $value];
+    }
+
+    public function getMostVisitedPlace()
+    {
+        $place = SpatialFeature::withCount('visitors')
+            ->orderBy('visitors_count', 'desc')
+            ->first();
+    
+        return "{$place->name}, {$place->visitors_count}";
+    }
+
     private function getStatistics(): array
     {
         $importers = app('importer.registry');
 
         return [
-            ['name' => 'Users', 'value' => User::count()],
-            ['name' => 'Places', 'value' => SpatialFeature::count()],
-            ['name' => 'Categories', 'value' => Category::count()],
-            ['name' => 'Importers', 'value' => count($importers)],
+            $this->statistic('Users', User::count()),
+            $this->statistic('Places', SpatialFeature::count()),
+            $this->statistic('Places with descriptions', SpatialFeature::whereNotNull('description')->count()),
+            $this->statistic('Categories', Category::count()),
+            $this->statistic('Importers', count($importers)),
+            $this->statistic('All visits', DB::table('visited_places')->count()),
+            $this->statistic('Visits today', DB::table('visited_places')->whereDate('created_at', today())->count()),
+            $this->statistic('Most visited place', $this->getMostVisitedPlace()),
         ];
     }
 }
